d300ab9867c3ec7a978d4b7f9a1dfa5d87f5070e
Lucas Viana <lucasvfv@gmail.com>
2022-01-28 23:16:59 -0300

    Versão Inicial - Botão Finalizado

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let pratoEscolhido = null;
+let bebidaEscolhida = null;
+let sobremesaEscolhida = null;
+
+let botaoPedido = document.querySelector(".button-pedido");
+botaoPedido.disabled = true;
 
 
 function clicadoPrato(nomePrato) {
-
     //Add um numero em frente ao "prato" para saber qual prato está sendo clicado
     for (let i = 1; i <= 3; i++) {
     
@@ -12,24 +17,32 @@ function clicadoPrato(nomePrato) {
 
             let check = document.querySelector('.icone-prato'+i);
             check.classList.toggle("escondido");
-            
-            console.log("toggle prato"+i); //  remover no commit
 
-            // Verifica e qual prato não ta sendo clicado e remove a borda dele
+            if(prato.classList.contains("borda-verde") === true){
+                  pratoEscolhido = "prato"+i;
+
+            }else{
+                 pratoEscolhido = null;
+
+            }
+
+            // Verifica qual prato não ta sendo clicado e remove a borda dele
             for (let j = 1; j <= 3; j++) {
                 if ( i != j){
                     let remove = document.querySelector('.prato'+j);
                     remove.classList.remove("borda-verde");
 
+
                     let removeIcon = document.querySelector('.icone-prato'+j);
                     removeIcon.classList.add("escondido");
 
-                    console.log("remove prato"+j); //  remover no commit
                 }
             }
         }
 
     }
+    mudarCorBotao();
+
 }
 function clicadoBebida(nomeBebida) {
 
@@ -44,8 +57,12 @@ function clicadoBebida(nomeBebida) {
             let check = document.querySelector('.icone-bebida'+i);
             check.classList.toggle("escondido");
             
-            
-            console.log("toggle bebida"+i); //  remover no commit
+            if(bebida.classList.contains("borda-verde") === true){
+                bebidaEscolhida = "bebida"+i;
+            }else{
+                bebidaEscolhida = null;
+
+            }
 
             // Verifica e qual prato não ta sendo clicado e remove a borda dele
             for (let j = 1; j <= 3; j++) {
@@ -56,12 +73,12 @@ function clicadoBebida(nomeBebida) {
                     let removeIcon = document.querySelector('.icone-bebida'+j);
                     removeIcon.classList.add("escondido");
 
-                    console.log("remove bebida"+j); //  remover no commit
                 }
             }
         }
 
     }
+    mudarCorBotao();
 }
 function clicadoSobremesa(nomeSobremesa) {
 
@@ -76,7 +93,11 @@ function clicadoSobremesa(nomeSobremesa) {
             let check = document.querySelector('.icone-sobremesa'+i);
             check.classList.toggle("escondido");
             
-            console.log("toggle sobremesa"+i); //  remover no commit
+            if(sobremesa.classList.contains("borda-verde") === true){
+                sobremesaEscolhida = "sobremesa"+i;
+            }else{
+                sobremesaEscolhida = null;
+            }
 
             // Verifica e qual prato não ta sendo clicado e remove a borda dele
             for (let j = 1; j <= 3; j++) {
@@ -88,10 +109,108 @@ function clicadoSobremesa(nomeSobremesa) {
                     removeIcon.classList.add("escondido");
 
 
-                    console.log("remove sobremesa"+j); //  remover no commit
                 }
             }
         }
 
     }
+    mudarCorBotao();
 }
+
+
+function estaSelecionado(pratoSelecionado, bebidaSelecionado, sobremesaSelecionado){
+    let isChecked;
+    if(pratoSelecionado !== null && bebidaSelecionado !== null && sobremesaSelecionado !== null){
+        isChecked = true;
+    }else{
+        isChecked = false;
+    }
+    return isChecked;
+}
+
+
+ function mudarCorBotao(){
+
+    if (estaSelecionado(pratoEscolhido, bebidaEscolhida,sobremesaEscolhida ) == true){
+        //Ativa o button
+        botaoPedido.disabled = false;
+
+        // Troca o design do button
+        let alterarBotao = document.querySelector(".button-pedido");
+        alterarBotao.classList.add("pedido-fechado");
+        // Altera o Texto do button
+        let alterarTextoBotao = document.querySelector(".fechar-pedido");
+        alterarTextoBotao.innerHTML = "Fechar Pedido";
+    }   else{
+        //Desativa o button
+        botaoPedido.disabled = true;
+
+        // Troca o design do button
+        let pedidoFechado = document.querySelector(".button-pedido");
+        pedidoFechado.classList.remove("pedido-fechado");
+
+        // Altera o Texto do button
+        let alterarTextoBotao = document.querySelector(".fechar-pedido");
+        alterarTextoBotao.innerHTML = "Selecione os 3 itens<br/> para fechar o pedido";
+    }
+
+        
+ }
+
+
+
+function validarPedido(){
+    console.log("Clicado");
+
+    let texto = "Olá, gostaria de fazer o pedido:\n - Prato: Frango Yin Yang \n - Bebida: Coquinha Gelada \n - Sobremesa: Pudim \n Total: R$ 27.70"
+
+    texto= window.encodeURIComponent(texto);
+    console.log(texto);
+
+
+    window.open("https://wa.me/5524998197200?text="+texto,'_blank')
+}
+
+
+
+
+
+
+
+// function clicadoPrato(nomePrato) {
+
+//     const selecionado = document.querySelector(".caixa-pratos .borda-verde");
+//     // Verifica quem está selecionado e remove a borda verde
+//     if(selecionado !== null){
+//         selecionado.classList.remove("borda-verde");
+//     }   
+//     // Add a borda verde
+//     let prato = document.querySelector("."+nomePrato);
+//     prato.classList.toggle("borda-verde");
+    
+
+//     let checkSelecionado = document.querySelector('.caixa-pratos .icone-'+nomePrato);
+//     checkSelecionado.classList.remove("escondido");
+
+// }
+
+
+// function clicadoBebida(nomeBebida) {
+
+//     const selecionado = document.querySelector(".caixa-bebidas .borda-verde");
+//     if(selecionado !== null){
+//         selecionado.classList.remove("borda-verde");
+//     }   
+//     let prato = document.querySelector(nomeBebida);
+//     prato.classList.toggle("borda-verde");
+// }
+
+// function clicadoSobremesa(nomeSobremesa) {
+
+//     const selecionado = document.querySelector(".caixa-sobremesa .borda-verde");
+//     if(selecionado !== null){
+//         selecionado.classList.remove("borda-verde");
+//     }   
+//     let prato = document.querySelector(nomeSobremesa);
+//     prato.classList.toggle("borda-verde");
+// }
